628d00f5ad21a51c4891f34cf6444af3e29baef3
ahmad_amirudin <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2020-01-19 05:32:44 +0700

    Initial Commit

--- animationlibrary/src/main/java/com/amier/animationlibrary/AnimationLibrary.java
+++ animationlibrary/src/main/java/com/amier/animationlibrary/AnimationLibrary.java
@@ -6,8 +6,12 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 public class AnimationLibrary {
-    public static void BounceAnimation(Context context, View targetView, double amplitudo, double frequency){
-        final Animation myAnim = AnimationUtils.loadAnimation(context, R.anim.bounce);
+    private Context mCtx;
+    AnimationLibrary(Context context){
+        mCtx = context;
+    }
+    public void BounceAnimation(View targetView, double amplitudo, double frequency){
+        final Animation myAnim = AnimationUtils.loadAnimation(mCtx, R.anim.bounce);
         BounceInterpolator interpolator = new BounceInterpolator(amplitudo,frequency);
         myAnim.setInterpolator(interpolator);
         targetView.startAnimation(myAnim);
